45aa654ad70debd49ee6983723fefb6acd8f827a
pharmile <nanaseprg@gmail.com>
2021-08-18 22:32:59 +0900

    ホーム機能

--- Controllers/home.php
+++ Controllers/home.php
@@ -3,6 +3,8 @@
 include_once '../config.php';
 // 便利な関数を読み込み
 include_once '../util.php';
+// ツイートデータ操作モデルを読み込む
+include_once '../Models/tweets.php';
 
 // TODO: ログインチェック
 $user = getUserSession();
@@ -15,30 +17,7 @@ if(!$user){
 // View用の変数
 $view_user = $user;
 //ツイート一覧
-$view_tweets = [
-[
-	'user_id' => 1,
-	'user_name' => 'taro',
-	'user_nickname' => '太郎',
-	'user_image_name' => 'sample-person.jpg',
-	'tweet_body' => '今プログラミングをしています。',
-	'tweet_image_name' => null,
-	'tweet_created_at' => '2021-03-15 14:00:00',
-	'like_id' => null,
-	'like_count' => 0,
-],
-[
-	'user_id' => 2,
-	'user_name' => 'jiro',
-	'user_nickname' => '次郎',
-	'user_image_name' => null,
-	'tweet_body' => 'コワーキングスペースをオープンしました！',
-	'tweet_image_name' => 'sample-post.jpg',
-	'tweet_created_at' => '2021-03-14 14:00:00',
-	'like_id' => 1,
-	'like_count' => 1,
-]
-];
+$view_tweets = findTweets($user);
 
 include_once '../Views/home.php';
 
